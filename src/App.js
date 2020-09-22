2b508b2e961735c3a3671ff2121153637a664bd2
bstripathy <68610648+bstripathy@users.noreply.github.com>
2020-09-22 11:55:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import SimpleFullCalender from "./components/calendar";
+
+function App() {
+  return <SimpleFullCalender />;
+}
+
+export default App;
---
+++ src/components/calendar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+
+export default function SimpleFullCalender(props) {
+  // const classes = useStyles();
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      weekends={false}
+      events={[
+        { title: "event 1", date: "2020-09-09" },
+        { title: "event 2", date: "2020-08-09" }
+      ]}
+    />
+  );
+}
